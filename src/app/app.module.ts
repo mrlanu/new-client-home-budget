445237306543c4f95d43c317932c5ef3e4ab7ec2
Serhiy K <mrlanu@gmail.com>
2019-07-05 23:00:01 -0500

    start operations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExpenceComponent } from './operations/expence/expence.component';
+import { OperationsComponent } from './operations/operations.component';
+import { IncomeComponent } from './operations/income/income.component';
+import { TransferComponent } from './operations/transfer/transfer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExpenceComponent,
+    OperationsComponent,
+    IncomeComponent,
+    TransferComponent
   ],
   imports: [
     BrowserModule,
