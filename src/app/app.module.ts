e01ca447f7bd6a689c0579e1b8324786a6b54fec
Serhiy K <mrlanu@gmail.com>
2019-07-06 23:16:31 -0500

    Datepicker, ExpenseComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ExpenceComponent } from './operations/expence/expence.component';
-import { OperationsComponent } from './operations/operations.component';
-import { IncomeComponent } from './operations/income/income.component';
-import { TransferComponent } from './operations/transfer/transfer.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ExpenceComponent} from './operations/expence/expence.component';
+import {OperationsComponent} from './operations/operations.component';
+import {IncomeComponent} from './operations/income/income.component';
+import {TransferComponent} from './operations/transfer/transfer.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,10 @@ import { TransferComponent } from './operations/transfer/transfer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/operations/expence/expence.component.ts
+++ src/app/operations/expence/expence.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {NgbDateAdapter, NgbDateNativeAdapter} from '@ng-bootstrap/ng-bootstrap';
+
+export interface AccountModel {
+  id: number;
+  type: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-expence',
   templateUrl: './expence.component.html',
-  styleUrls: ['./expence.component.css']
+  styleUrls: ['./expence.component.css'],
+  providers: [{provide: NgbDateAdapter, useClass: NgbDateNativeAdapter}]
 })
 export class ExpenceComponent implements OnInit {
 
-  constructor() { }
+  accounts: AccountModel[] = [{id: 1, name: 'Chase', type: 'Saving'}, {id: 2, name: 'BofA', type: 'Saving'}];
+  expenseForm: FormGroup;
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.expenseForm = new FormGroup({
+      date: new FormControl(new Date()),
+      amount: new FormControl(0.0),
+      account: new FormControl('Account...'),
+      category: new FormControl(),
+      subcategory: new FormControl(),
+      description: new FormControl()
+    });
+  }
+
+  onSubmit() {
+    console.log(this.expenseForm.value);
   }
 
 }
