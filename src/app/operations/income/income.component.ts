924b7e2ccc9d1a61a37f0ddb7b69bc477ec7f9f5
Serhiy K <mrlanu@gmail.com>
2019-07-07 20:12:25 -0500

    Income & Transfer Components

--- src/app/operations/income/income.component.ts
+++ src/app/operations/income/income.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {AccountModel} from '../expence/expence.component';
+import {NgbDateAdapter, NgbDateNativeAdapter} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-income',
   templateUrl: './income.component.html',
-  styleUrls: ['./income.component.css']
+  styleUrls: ['./income.component.css'],
+  providers: [{provide: NgbDateAdapter, useClass: NgbDateNativeAdapter}]
 })
 export class IncomeComponent implements OnInit {
 
-  constructor() { }
+  accounts: AccountModel[] = [{id: 1, name: 'Chase', type: 'Saving'}, {id: 2, name: 'BofA', type: 'Saving'}];
+  expenseForm: FormGroup;
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.expenseForm = new FormGroup({
+      date: new FormControl(new Date()),
+      amount: new FormControl(0.0),
+      account: new FormControl('Account...'),
+      category: new FormControl(),
+      subcategory: new FormControl(),
+      description: new FormControl()
+    });
+  }
+
+  onSubmit() {
+    console.log(this.expenseForm.value);
   }
 
 }
--- src/app/operations/transfer/transfer.component.ts
+++ src/app/operations/transfer/transfer.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import {AccountModel} from '../expence/expence.component';
+import {FormControl, FormGroup} from '@angular/forms';
+import {NgbDateAdapter, NgbDateNativeAdapter} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-transfer',
   templateUrl: './transfer.component.html',
-  styleUrls: ['./transfer.component.css']
+  styleUrls: ['./transfer.component.css'],
+  providers: [{provide: NgbDateAdapter, useClass: NgbDateNativeAdapter}]
 })
 export class TransferComponent implements OnInit {
 
-  constructor() { }
+  accounts: AccountModel[] = [{id: 1, name: 'Chase', type: 'Saving'}, {id: 2, name: 'BofA', type: 'Saving'}];
+  expenseForm: FormGroup;
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.expenseForm = new FormGroup({
+      date: new FormControl(new Date()),
+      amount: new FormControl(0.0),
+      accountFrom: new FormControl('Account...'),
+      accountTo: new FormControl('Account...')
+    });
+  }
+
+  onSubmit() {
+    console.log(this.expenseForm.value);
   }
 
 }
