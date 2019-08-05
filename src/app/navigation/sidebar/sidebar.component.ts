7a95d5aee9fbe35f084c789796ff4c033e511048
Serhiy K <mrlanu@gmail.com>
2019-08-04 22:21:16 -0500

    environment changed

--- src/app/navigation/sidebar/sidebar.component.ts
+++ src/app/navigation/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {UtilityService} from '../../services/utility.service';
 import {Subscription} from 'rxjs';
+import {AuthService} from '../../auth/auth.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -12,13 +13,17 @@ export class SidebarComponent implements OnInit, OnDestroy {
   randomUser: {name: string, image: string} = {name: '', image: ''};
   componentSubs: Subscription[] = [];
 
-  constructor(private utilityService: UtilityService) { }
+  constructor(private utilityService: UtilityService, private authService: AuthService) { }
 
   ngOnInit() {
     this.componentSubs.push(this.utilityService.getRandomUser()
       .subscribe(res => {
         this.randomUser = res;
       }));
+  }
+
+  onLogout() {
+    this.authService.logout();
   }
 
   ngOnDestroy(): void {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'https://lanu-home-budget.herokuapp.com',
+  budgetId: 0
 };
