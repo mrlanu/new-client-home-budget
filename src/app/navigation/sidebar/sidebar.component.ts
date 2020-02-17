c10291d60980356aa383e5c7213ccdb66300090f
mrlanu <mrlanu@gmail.com>
2020-02-17 15:24:46 -0600

    feat: fetch image direct to src
    change the way to fetching image without httpClient

--- src/app/navigation/sidebar/sidebar.component.ts
+++ src/app/navigation/sidebar/sidebar.component.ts
@@ -13,23 +13,14 @@ export class SidebarComponent implements OnInit, OnDestroy {
   randomUser: {name: string, image: string} = {name: '', image: ''};
   componentSubs: Subscription[] = [];
 
-  imageSrc: any = '';
-
   constructor(private utilityService: UtilityService,
               private authService: AuthService) { }
 
   ngOnInit() {
-    this.getImageFromService();
     /*this.componentSubs.push(this.utilityService.getRandomUser()
       .subscribe(res => {
         this.randomUser = res;
       }));*/
-  }
-
-  getImageFromService() {
-    this.utilityService.downloadProfileImage().subscribe(response => {
-      this.imageSrc = 'data:image/jpeg;base64,' + response;
-    });
   }
 
   onLogout() {
