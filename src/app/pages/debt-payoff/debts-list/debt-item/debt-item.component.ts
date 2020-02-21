3325c88277fa5232bc46583e2f3a9d97d7948695
mrlanu <mrlanu@gmail.com>
2020-02-21 16:55:05 -0600

    refactor: finish DebtItem view

--- src/app/pages/debt-payoff/debts-list/debt-item/debt-item.component.ts
+++ src/app/pages/debt-payoff/debts-list/debt-item/debt-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {DebtModel} from '../../../../models/debt.model';
+import {faEdit, faTimes} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-debt-item',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DebtItemComponent implements OnInit {
 
+  @Input() debtItem: DebtModel;
+  progress = 0;
+  faEdit = faEdit;
+  faTimes = faTimes;
+
   constructor() { }
 
   ngOnInit() {
+    this.progress = (this.debtItem.startBalance - this.debtItem.currentBalance) * 100 / this.debtItem.startBalance;
   }
 
 }
--- src/app/pages/debt-payoff/debts-list/debts-list.component.ts
+++ src/app/pages/debt-payoff/debts-list/debts-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {DebtModel} from '../../../models/debt.model';
 
 @Component({
   selector: 'app-debts-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./debts-list.component.css']
 })
 export class DebtsListComponent implements OnInit {
+
+  debtsList: DebtModel[] =
+    [{name: 'City VISA', apr: 12, currentBalance: 1870, minimumPayment: 100, nextPaymentDue: new Date(),
+      paymentsList: [], startBalance: 2000},
+    {name: 'BofA MASTER CARD TEST TEST', apr: 12, currentBalance: 1000, minimumPayment: 100, nextPaymentDue: new Date(),
+      paymentsList: [{amount: 20, date: new Date()}], startBalance: 2000}];
 
   constructor() { }
 
